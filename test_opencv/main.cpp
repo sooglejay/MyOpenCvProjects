392814fe856a2772fc0fe78f410b109683ca5e16
sooglejay <sooglejay@gmail.com>
2016-01-11 22:17:04 +0800

    Initial Commit

---
+++ test_opencv/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  test_opencv
+//
+//  Created by sooglejay on 16/1/11.
+//  Copyright © 2016年 sooglejay. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
